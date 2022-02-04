fb58a76072862f5e604f110fffc4c528121058a9
bool <booleancoercion@gmail.com>
2022-02-04 22:52:28 +0200

    Started work on the string lexer

--- src/lex/string.rs
+++ src/lex/string.rs
@@ -1,1 +1,31 @@
-enum StringToken {}
+use logos::{Lexer, Logos};
+
+pub enum ParseLitError {
+    InvalidEscape,
+    UnescapedNewline,
+}
+
+pub fn parse_short_string(lit: &str) -> Result<Vec<u8>, ParseLitError> {
+    todo!()
+}
+
+#[derive(Copy, Clone, Debug, Logos)]
+enum StringToken {
+    #[regex(r"\\.")]
+    Escape,
+
+    #[regex(r"\\(\n\r|\r\n|\n)")]
+    EscapedLineBreak,
+
+    #[regex(r"\\x[[:xdigit:]][[:xdigit:]]")]
+    HexByte,
+
+    #[regex(r"\\\d\d?\d?", priority = 4)]
+    DecByte,
+
+    #[regex(r"\\u\{[[:xdigit:]]+\}")]
+    Utf8,
+
+    #[error]
+    Error,
+}
